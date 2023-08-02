0b76502929bb4ea3fd6d0c78af1e2f6d1aab1c3c
wedgon6 <117192830+wedgon6@users.noreply.github.com>
2023-08-02 22:07:12 +0300

    02.08
    продолжаем делать переходы между сценами

--- Assets/Scripts/UI/MainMenu/Menu.cs
+++ Assets/Scripts/UI/MainMenu/Menu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
@@ -17,5 +18,10 @@ public class Menu : MonoBehaviour
     public void ExitGameButton()
     {
         Application.Quit();
+    }
+
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene("Battleground");
     }
 }
